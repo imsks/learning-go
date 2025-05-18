abb9a947410f283034405cee3a440d7f37043918
Sachin <sachinkshuklaoo7@gmail.com>
2025-05-18 22:34:04 +0530

    Add learning-go file and update main.go with commented code examples and a map demonstration

--- main.go
+++ main.go
@@ -3,5 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
+
+	// var a, b string
+
+	// fmt.Println(a, b)
+	// const a = 5
+	// fmt.Println(a)
+
+	// var i = 2
+
+	// for i < 5 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// var a [5]int
+
+	// b := [5]int{1, 2, 3, 4, 5}
+
+	// for i, v := range b {
+	// 	fmt.Println(i, v)
+	// }
+
+	// Maps
+	emailSent := make(map[string]int)
+
+	emailSent["sendgrid"] = 100
+	emailSent["mailgun"] = 25
+
+	// fmt.Println(emailSent["sendgrids"])
+
+	for provider, count := range emailSent {
+		fmt.Println(provider, '-', count)
+	}
 }
