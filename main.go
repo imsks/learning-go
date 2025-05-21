1ddb6df6132783b3c6f86343e82f69ed2750030a
Sachin <sachinkshuklaoo7@gmail.com>
2025-05-21 09:00:22 +0530

    Refactor main.go to include EmailRequest struct and implement email sending functionality; add utils/email.go for email utility function.

--- main.go
+++ main.go
@@ -3,38 +3,88 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("Hello World")
-
-	// var a, b string
-
-	// fmt.Println(a, b)
-	// const a = 5
-	// fmt.Println(a)
-
-	// var i = 2
-
-	// for i < 5 {
-	// 	fmt.Println(i)
-	// 	i++
-	// }
-
-	// var a [5]int
-
-	// b := [5]int{1, 2, 3, 4, 5}
-
-	// for i, v := range b {
-	// 	fmt.Println(i, v)
-	// }
-
-	// Maps
-	emailSent := make(map[string]int)
-
-	emailSent["sendgrid"] = 100
-	emailSent["mailgun"] = 25
-
-	// fmt.Println(emailSent["sendgrids"])
-
-	for provider, count := range emailSent {
-		fmt.Println(provider, '-', count)
+	type EmailRequest struct {
+		To      string
+		Subject string
+		HTML    string
 	}
+
+	email := EmailRequest{
+		To:      "hello@tbe.in",
+		Subject: "Welcome to Chitthi",
+		HTML:    "<h1>Hello World</h1>",
+	}
+
+	fmt.Println(email.HTML)
+
 }
+
+// func main() {
+// 	// fmt.Println("Hello World")
+
+// 	// var a, b string
+
+// 	// fmt.Println(a, b)
+// 	// const a = 5
+// 	// fmt.Println(a)
+
+// 	// var i = 2
+
+// 	// for i < 5 {
+// 	// 	fmt.Println(i)
+// 	// 	i++
+// 	// }
+
+// 	// var a [5]int
+
+// 	// b := [5]int{1, 2, 3, 4, 5}
+
+// 	// for i, v := range b {
+// 	// 	fmt.Println(i, v)
+// 	// }
+
+// 	// Maps
+// 	// emailSent := make(map[string]int)
+
+// 	// emailSent["sendgrid"] = 100
+// 	// emailSent["mailgun"] = 25
+
+// 	// // fmt.Println(emailSent["sendgrids"])
+
+// 	// for provider, count := range emailSent {
+// 	// 	fmt.Println(provider, '-', count)
+// 	// }
+
+// 	// fmt.Println(sum(1, 2))
+
+// 	result, err := divide(10, 2)
+
+// 	if err != nil {
+// 		fmt.Println("Error: ", err)
+// 	} else {
+// 		fmt.Println("Result: ", result)
+// 	}
+
+// 	square := func(x int) int {
+// 		return x * x
+// 	}
+
+// 	fmt.Println(square(2))
+// }
+
+// // Functions
+// // func sayHello() {
+// // 	fmt.Println("Hello")
+// // }
+
+// // func sum(a int, b int) int {
+// // 	return a + b
+// // }
+
+// func divide(a, b float64) (float64, error) {
+// 	if b == 0 {
+// 		return 0, fmt.Errorf("cannot divide by zero")
+// 	}
+
+// 	return a / b, nil
+// }
---
+++ utils/email.go
@@ -0,0 +1,5 @@
+package utils
+
+func sendEmail(to string) string {
+	return "mailto:" + to
+}
